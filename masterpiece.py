1a316f4afd330e3757d44ccd40aa8773203d4763
cluster777 <davidlaksmanasan@gmail.com>
2021-09-12 16:17:15 +0700

    fix headless

--- masterpiece.py
+++ masterpiece.py
@@ -13,7 +13,7 @@ chrome_options.add_argument("--headless")
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
 chrome_options.add_argument('user-agent={0}'.format(user_agent))
 chrome_options.add_argument('window-size=1920x1080');
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 # slow start
 time.sleep(10)
 #get the page
