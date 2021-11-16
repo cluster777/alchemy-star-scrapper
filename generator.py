2e9cf6a0cfa0f1d47eb87331776f3718b2bdd880
cluster777 <davidlaksmanasan@gmail.com>
2021-11-16 15:58:45 +0700

    fix comma dangle

--- generator.py
+++ generator.py
@@ -1,21 +1,15 @@
 import json
 def generate_charlist(data):
     with open("./json/charlist.json", "w") as f:
-        f.write('[')
         for char in data:
-            if(char["name"]==""):
-                break
-            f.write("{")
-            f.write("  \"char_name\":\"{name}\",".format(name=char["name"]))
-            f.write("  \"element_main\":\"{ele}\",".format(ele=char["element_main"]))
-            f.write("  \"element_sub\":\"{ele}\",".format(ele=char["element_sub"]))
-            f.write("  \"faction\":\"{fac}\",".format(fac=char["faction"]))
-            f.write("  \"class\":\"{clas}\",".format(clas=char["class"]))
-            f.write("  \"char_rarity\":\"{rare}\"".format(rare=char["rarity"]))
-            f.write('},')
-            
-        f.write(']')
 
+            char["char_name"] = char.pop("name")
+            char["char_rarity"] = char.pop("rarity")
+        json.dump(data[:-1], f)
+    for char in data:
+
+        char["name"] = char.pop("char_name")
+        char["rarity"] = char.pop("char_rarity")
 def generate_char(data):
     with open("./json/{name}.json".format(name=data["name"]), "w") as f:
-        json.dump(data, f, indent = 6)
+        json.dump(data, f,indent=1)
